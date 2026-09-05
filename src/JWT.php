422f391cdeb727ecddaf77d03955f2f5a83daf30
Tomas <tomas@.dev.co>
2026-09-05 20:13:44 +0200

    composer.json a jtw.php added

---
+++ src/JWT.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fuckbase\JWT;
+
+class JWT
+{
+    /**
+     * Testovací metoda pro ověření funkčnosti knihovny
+     */
+    public static function hi(): string
+    {
+        return "Hi from Fuckbase";
+    }
+}
